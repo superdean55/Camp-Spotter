5ec0f4eafe5c62561c63546eaeb9bcbdb10f220b
superdean55 <superdean55@gmail.com>
2023-05-12 19:39:51 +0200

    first commit

---
+++ app/src/main/java/hr/ferit/dejanmihic/campspotter/Registration.kt
@@ -0,0 +1,11 @@
+package hr.ferit.dejanmihic.campspotter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
